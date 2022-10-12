845f1c8f2e652d36040975e470a2f26a07a40c73
Leograzioli <leograzioli@gmail.com>
2022-10-12 15:26:38 +0200

    bonus 2

--- js/script.js
+++ js/script.js
@@ -17,50 +17,79 @@ for (let i = 0; i < pictureLits.length; i++) {
     card.innerHTML += element;
 }
 
-//initial status 
+const cardLeft = document.querySelector(".card-left");
+
+for (let i = 0; i < pictureLits.length; i++) {
+    const picPosition = pictureLits[i];
+    const element = 
+    `<div class="card-small-img ">
+        <img src="${picPosition}" alt="">
+    </div>`;
+    cardLeft.innerHTML += element;
+}
+
+
+//initial status of active class
 const cardImg = document.getElementsByClassName("card-img");
 let sliderPosition = 0;
 cardImg[sliderPosition].classList.add("active");
 
-//to hide and show pics on click
+//initial status of active-border class
+const borderImg = document.getElementsByClassName("card-small-img");
+let borderPosition = 0
+borderImg[borderPosition].classList.add("active-border");
+
+//to add and take off class active on click
 const topButton = document.querySelector(".top");
 const bottomButton = document.querySelector(".bottom");
 console.log(topButton, bottomButton);
 
-
-//button active
 topButton.addEventListener("click", function() {
 
     if (sliderPosition < 4){
         cardImg[sliderPosition].classList.remove("active");
+        borderImg[borderPosition].classList.remove("active-border");
     
         sliderPosition++;
+        borderPosition++;
         
         cardImg[sliderPosition].classList.add("active");
+        borderImg[borderPosition].classList.add("active-border");
 
     } else {
         cardImg[sliderPosition].classList.remove("active");
+        borderImg[borderPosition].classList.remove("active-border");
     
         sliderPosition = 0;
+        borderPosition = 0
         
         cardImg[sliderPosition].classList.add("active");
+        borderImg[borderPosition].classList.add("active-border");
     }
 });
 
 bottomButton.addEventListener("click", function() {
     if (sliderPosition > 0) {
         cardImg[sliderPosition].classList.remove("active");
+        borderImg[borderPosition].classList.remove("active-border");
     
         sliderPosition--;
+        borderPosition--;
         
         cardImg[sliderPosition].classList.add("active");
+        borderImg[borderPosition].classList.add("active-border");
 
     } else {
         cardImg[sliderPosition].classList.remove("active");
+        borderImg[borderPosition].classList.remove("active-border");
     
         sliderPosition = 4;
+        borderPosition = 4;
         
         cardImg[sliderPosition].classList.add("active");
+        borderImg[borderPosition].classList.add("active-border");
     }
 })
+
+
 
