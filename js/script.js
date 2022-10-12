f55f5ab898e61ab72d957a1f2b2f9830ab4f489a
Leograzioli <leograzioli@gmail.com>
2022-10-12 14:37:45 +0200

    milestone 2 e 3

---
+++ js/script.js
@@ -0,0 +1,52 @@
+const pictureLits = [
+    "img/01.jpg", 
+    "img/02.jpg", 
+    "img/03.jpg", 
+    "img/04.jpg", 
+    "img/05.jpg",
+];
+const card = document.querySelector(".card");
+
+//create element 
+for (let i = 0; i < pictureLits.length; i++) {
+    const picPosition = pictureLits[i];
+    const element = 
+    `<div class="card-img ">
+        <img src="${picPosition}" alt="">
+    </div>`;
+    card.innerHTML += element;
+}
+
+//initial status 
+const cardImg = document.getElementsByClassName("card-img");
+console.log(cardImg)
+let sliderPosition = 0;
+console.log(sliderPosition);
+cardImg[sliderPosition].classList.add("active");
+
+//to hide and show pics on click
+const topButton = document.querySelector(".top");
+const bottomButton = document.querySelector(".bottom");
+console.log(topButton, bottomButton);
+
+topButton.addEventListener("click", function() {
+
+    if (sliderPosition < 4){
+        cardImg[sliderPosition].classList.remove("active");
+    
+        sliderPosition++;
+        
+        cardImg[sliderPosition].classList.add("active");
+    }
+});
+
+bottomButton.addEventListener("click", function() {
+    if (sliderPosition > 0) {
+        cardImg[sliderPosition].classList.remove("active");
+    
+        sliderPosition--;
+        
+        cardImg[sliderPosition].classList.add("active");
+    }
+})
+
